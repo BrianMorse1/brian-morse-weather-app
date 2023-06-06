b4f3b6cab0c80de6558564b5187a4d69e78c56c2
Brian Morse <brian.p.morse@gmail.com>
2023-06-06 10:45:50 -0700

    I have functions to retrieve geocoding and weather API information functioning. Now I need to send the correct information to the correct places in the DOM

--- index.js
+++ index.js
@@ -1,27 +1,59 @@
 //requiring dotenv file for key security.
-require('dotenv').config();
+//require('dotenv').config();
 
 //keys for mapquest (geocoding) and openweather api's. 
-const mapquestKey = process.env.mapquestKey;
-const openweatherKey = process.env.mapquestKey;
+const mapquestKey = 'jyim88NKRLxKX4nc7UXSUsDTb138aGkk';
+const openweatherKey = "47949a277751c0fcf4d5c60f6eb3cd58";
 
 // variables for accessing user input from search form
 const searchElement = document.querySelector('#search-input');
-const userInput = searchElement.value;
 const submit = document.querySelector('#submit');
+
+//variables for DOM elements to be populated with information
+let forecast = document.querySelector('#five-day');
+let pastSearches = document.querySelector('#past-searches');
+let cityName = document.querySelector('#city-name');
+let currentDate = document.querySelector('#current-date');
+let currentIcon = document.querySelector('#current-weather-icon');
+let currentTemp = document.querySelector('#current-temperature');
+let currentHumidity = document.querySelector('#current-humidity');
+let currentWind = document.querySelector('#current-wind');
+
+
 
 //variables for setting lat and long of user searches
 let lat;
 let long;
 
-const formHandler = () => {
-    preventDefault();
+const getGeo = () => {
+    const userInput = searchElement.value;
 
     //fetch's geocoding data from Mapquestapi
-    fetch(`https://www.mapquestapi.com/geocoding/v1/address?key=${mapquestKey}&location=${userInput}`)
+    return fetch(`https://www.mapquestapi.com/geocoding/v1/address?key=${mapquestKey}&location=${userInput}`)
     .then(response => response.json())
     .then(data => { 
-        console.log(data)
-    })};
+        lat = data.results[0].locations[0].latLng.lat;
+        long = data.results[0].locations[0].latLng.lng;
+        })
+};
+    
+        //uses data from geocoding for fetch request to open weather api
+        const getWeather = async () => {
+            try {
+              const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${openweatherKey}`);
+              const data = await response.json();
+              console.log(data.list[0].main.temp);
+            } catch (error) {
+              console.log(error);
+            }
+          };
+          
 
-    addEventListener(submit, formHandler);
+    //function to call both previous functions
+    const formHandler = async (event) =>{
+        event.preventDefault();
+        await getGeo();
+        getWeather();
+    }
+
+    submit.addEventListener('click', formHandler);
